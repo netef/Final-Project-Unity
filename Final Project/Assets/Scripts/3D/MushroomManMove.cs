556e0c0a71c43c1b99b951c9ef700242e5d42c69
guy <guymoreh11@gmail.com>
2019-07-05 20:01:16 +0300

    Death update

---
+++ Final Project/Assets/Scripts/3D/MushroomManMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomManMove : MonoBehaviour
+{
+    float left = 13.3f, right = 30f;
+    int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(direction == 1 )
+        {
+            transform.Translate(Vector3.right * 2 * Time.deltaTime, Space.World);
+            direction = 1;
+        }
+     if(transform.position.x > right)
+        {
+            direction = 2;
+        }
+        if (direction == 2)
+        {
+            transform.Translate(Vector3.right * -2 * Time.deltaTime, Space.World);
+            direction = 2;
+        }
+        if (transform.position.x < left)
+        {
+            direction = 1;
+        }
+    }
+}
---
+++ Final Project/Assets/Scripts/3D/Pipe002.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe002 : MonoBehaviour
+{
+    bool StoodOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoodOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(StoodOn)
+        {
+            
+            GameObject.Find("MaleFreeSimpleMovement1").GetComponent<SimpleCharacterControl>().enabled = true;
+            GameObject.Find("MaleFreeSimpleMovement1").transform.position = new Vector3(31.97f, 6.01f, 11.24f);//(47f, 5.4f   , 12.8f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        StoodOn = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        StoodOn = true;
+    }
+   
+}
