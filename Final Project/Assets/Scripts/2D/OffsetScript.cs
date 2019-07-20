d3a9057b687b8a6d54f8e192dc9255cc86afe725
netef <netef10@gmail.com>
2019-07-20 16:43:32 +0300

    added main menu and game over

---
+++ Final Project/Assets/Scripts/2D/OffsetScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OffsetScript : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        GetComponent<Image>().materialForRendering.mainTextureOffset = new Vector2(Time.time * speed, 0);
+    }
+}
---
+++ Final Project/Assets/Scripts/Menus/HowToPlayScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayScript : MonoBehaviour
+{
+    public GameObject instructions;
+    bool show;
+    void Start()
+    {
+        show = false;
+    }
+
+    void Update()
+    {
+        instructions.SetActive(show);
+    }
+
+    public void Click()
+    {
+        show = !show;
+    }
+}
