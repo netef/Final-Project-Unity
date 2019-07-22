c686f974180a2e04cb1dd7e739d3c55a89b006fb
netef <netef10@gmail.com>
2019-07-22 18:11:51 +0300

    added minimap to 3d, added boss level

---
+++ Final Project/Assets/Scripts/2D/AxeScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeScript : MonoBehaviour
+{
+    public Transform leftPos, rightPos;
+    public GameObject mario, boss, bridge;
+
+    int counter;
+
+    void Start()
+    {
+        counter = 3;
+    }
+    void Update()
+    {
+        if (counter == 0)
+            Win();
+    }
+
+    void Win()
+    {
+        mario.GetComponent<Animator>().enabled = false;
+        mario.GetComponent<MarioScriptNew>().enabled = false;
+        boss.GetComponent<BossScript>().enabled = false;
+        Destroy(bridge);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && transform.position == rightPos.position)
+        {
+            transform.position = leftPos.position;
+            counter--;
+        }
+
+        else if (collision.gameObject.CompareTag("Player") && transform.position == leftPos.position)
+        {
+            transform.position = rightPos.position;
+            counter--;
+        }
+    }
+}
---
+++ Final Project/Assets/Scripts/2D/BossEnterScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnterScript : MonoBehaviour
+{
+    public GameObject wall1, wall2, mario, mainCamera, bossCamera;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            mario.GetComponent<MarioScriptNew>().enabled = false;
+            mario.GetComponent<Animator>().enabled = false;
+            mainCamera.SetActive(false);
+            bossCamera.SetActive(true);
+            wall1.SetActive(true);
+            wall2.SetActive(true);
+            StartCoroutine(Wait());
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
+        mario.GetComponent<MarioScriptNew>().enabled = true;
+        mario.GetComponent<Animator>().enabled = true;
+        Destroy(gameObject);
+    }
+}
