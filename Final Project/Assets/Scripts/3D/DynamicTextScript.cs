e2af74c3c70c2600b826f670838de453691dabdb
netef <netef10@gmail.com>
2019-07-26 14:25:00 +0300

    added backgrounds, added music to 3d

---
+++ Final Project/Assets/Scripts/3D/DynamicTextScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicTextScript : MonoBehaviour
+{
+    public GameObject text;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            text.SetActive(true);
+            Destroy(text, 3);
+        }
+    }
+}
